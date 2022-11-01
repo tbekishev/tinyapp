f842bd497529e73ebd92b130a1379c1bd4eb3ad7
Tuigun Bekishev <tbekishev@gmail.com>
2022-10-31 23:17:08 -0300

    POST method added

--- express_server.js
+++ express_server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
-app.set("view engine", "ejs");
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -44,6 +45,10 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
