fc556b49482abd25eb581f08c97b099e8a03b0ca
Tuigun Bekishev <tbekishev@gmail.com>
2022-11-01 19:27:58 -0300

    url edit added

--- express_server.js
+++ express_server.js
@@ -72,6 +72,12 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect('/urls');
 });
 
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL;
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
