274ecd827ea180c0e1167cd7b029926eb6b33221
Tuigun Bekishev <tbekishev@gmail.com>
2022-10-31 22:03:22 -0300

    Filling Out the urls_index

--- express_server.js
+++ express_server.js
@@ -35,6 +35,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: "https://www.canada.ca" };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
