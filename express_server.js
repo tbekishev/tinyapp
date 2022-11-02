bd93e104272424cc54c61f6182334496d40981b2
Tuigun Bekishev <tbekishev@gmail.com>
2022-11-02 14:45:12 -0300

    urls_register added

--- express_server.js
+++ express_server.js
@@ -85,6 +85,15 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 })
 
+app.get("/register", (req, res) => {
+  const templateVars = { username: req.cookies.username };
+  res.render("urls_register", templateVars);
+});
+
+app.post("/register", (req, res) => {
+  res.redirect("/urls");
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
